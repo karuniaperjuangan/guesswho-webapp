238012d69d81e1de3ee68e739b79cc1f74a26a8b
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2024-10-19 22:36:12 +0700

    overhaul ui

--- svelte-frontend/src/store.ts
+++ svelte-frontend/src/store.ts
@@ -4,3 +4,4 @@ import { writable } from 'svelte/store';
 export const media = writable<File|null>(null);
 export const status = writable('');
 export const result = writable<Face[]>([]);
+export const activeFace = writable<Face|null>(null);
